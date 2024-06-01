073738381024eb5ef3f720acfb77f6282450978e
muhammad-muslim <124064868+muhammad-muslim@users.noreply.github.com>
2024-06-01 13:14:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -18,24 +18,26 @@ document.getElementById("toggle").addEventListener("click", function () {
   }
 });
 
-
-
-
 function switchTab(tabId) {
+  const selectedTab = document.getElementById(tabId);
+  const isActive = selectedTab.classList.contains("active");
+
   const tabs = document.querySelectorAll(".tab-content");
   tabs.forEach((tab) => tab.classList.remove("active"));
 
   const tabButtons = document.querySelectorAll(".tab");
   tabButtons.forEach((button) => button.classList.remove("active"));
 
-  const selectedTab = document.getElementById(tabId);
-  selectedTab.classList.add("active");
+  if (!isActive) {
+    selectedTab.classList.add("active");
 
-  const selectedTabButton = document.querySelector(
-    `.tab[data-tab="${tabId}"]`
-  );
-  selectedTabButton.classList.add("active");
+    const selectedTabButton = document.querySelector(
+      `.tab[data-tab="${tabId}"]`
+    );
+    selectedTabButton.classList.add("active");
+  }
 }
+
 document.querySelectorAll(".tab").forEach((tabElement) => {
   tabElement.addEventListener("click", function (event) {
     const tabId = event.currentTarget.getAttribute("data-tab");
@@ -43,3 +45,30 @@ document.querySelectorAll(".tab").forEach((tabElement) => {
   });
 });
 
+
+
+
+
+
+function myfunc() {
+  var navbar = document.getElementById('navbar');
+  navbar.classList.toggle('show');
+}
+
+window.onscroll = function () {
+  stickyNavbar();
+
+};
+
+
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function stickyNavbar() {
+  if (window.pageYOffset > sticky) {
+      navbar.classList.add("sticky-navbar");
+  }
+  else {
+      navbar.classList.remove("sticky-navbar");
+  }
+}
